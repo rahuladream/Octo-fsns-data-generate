0e07e360b5cfa81c684e1eafa37bdebf8bcb6547
Rahul Singh <rahul.singh4800@gmail.com>
2020-01-28 15:51:29 +0530

    dict and byte feature

--- tfrecord_generate.py
+++ tfrecord_generate.py
@@ -9,6 +9,8 @@ import PIL.Image as Image
 Main moto to convert tf_convert from fsns record file
 """
 
+dictionary_path = 'dict_classes.txt'
+
 def encoded_utf8_string(text, length, dic, null_char_id=5462):
     """
     Encode the string and also take care of null characters
@@ -21,3 +23,16 @@ def encoded_utf8_string(text, length, dic, null_char_id=5462):
         char_ids_unpadded[i]= hash_id
 
     return char_ids_padded, char_ids_unpadded
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+dict_values = {}
+with open(dictionary_path, encoding="utf8") as dict_file:
+    for line in dict_file:
+        (key, value) = line.strip.split('\t')
+        dict_values[value] = int(key)
+print(dict_values)
